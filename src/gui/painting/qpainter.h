35c67a8f6c9d707c8c5b88eb970f9256bb3b1d47
James Emerton <jemerton@spiresystems.com>
2014-11-27 08:22:17 -0800

    Fixes compilation error

--- src/gui/painting/qpainter.h
+++ src/gui/painting/qpainter.h
@@ -455,9 +455,9 @@ public:
     inline void addLink(const QRect &r, const QString &anchor);
     void addLink(const QRectF &r, const QString &anchor);
     
-    void addTextField(const QRectF &r, const QString &text="", const QString &name="", bool multiLine=false, bool password=false, bool readOnly=false, int maxLength=-1);
-    void addCheckBox(const QRectF &r, bool checked=false, const QString &name="", bool readOnly=false);
-    void addRadioButton(const QRectF &r, const QString & group="",  bool checked=false, const QString &name="", bool readOnly=false);;
+    void addTextField(const QRectF &r, const QString &text=QString(), const QString &name=QString(), bool multiLine=false, bool password=false, bool readOnly=false, int maxLength=-1);
+    void addCheckBox(const QRectF &r, bool checked=false, const QString &name=QString(), bool readOnly=false);
+    void addRadioButton(const QRectF &r, const QString & group=QString(),  bool checked=false, const QString &name=QString(), bool readOnly=false);;
 
     inline void addHyperlink(int x, int y, int w, int h, const QUrl &url);
     inline void addHyperlink(const QRect &r, const QUrl &url);
